8c084d9caa234db2f3c7c463abb556de7a61312d
Colin Keating <ckeating3000@gmail.com>
2016-10-10 09:59:11 -0500

    Initial Commit

---
+++ Data_Extraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data_Extraction
+//
+//  Created by Colin Keating on 10/10/16.
+//  Copyright © 2016 Colin Keating. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
